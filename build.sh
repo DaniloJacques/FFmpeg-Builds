17f101809388f7d9362ff50894461e7aebf41f47
DaniloJacques <danilo.jacques@gmail.com>
2026-01-17 11:48:19 -0300

    Update build.sh to Zen3

--- build.sh
+++ build.sh
@@ -19,9 +19,9 @@ fi
 rm -rf ffbuild
 mkdir ffbuild
 
-FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/MartinEesmaa/FFmpeg.git}"
+FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/DaniloJacques/FFmpeg.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
-GIT_BRANCH="${GIT_BRANCH:-master}"
+GIT_BRANCH="${GIT_BRANCH:-zen3}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
 
 BUILD_SCRIPT="$(mktemp)"
