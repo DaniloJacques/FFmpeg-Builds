f0c53aa5731ac3917dc6289262749bedb918e384
DaniloJacques <danilo.jacques@gmail.com>
2026-03-13 15:00:59 -0300

    chore: port isolated SVT-AV1 & x265 clang build technique from zen3-clang to tigerlake-clang

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -16,13 +16,43 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     mkdir build && cd build
 
-    local LTO_FLAG="-DSVT_AV1_LTO=ON"
+    cp "$FFBUILD_CMAKE_TOOLCHAIN" "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_C_COMPILER clang)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_CXX_COMPILER clang++)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_AR llvm-ar)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_RANLIB llvm-ranlib)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_NM llvm-nm)' >> "$PWD/clang_toolchain.cmake"
+
+    # Build clean CFLAGS for Clang based on RAW_CFLAGS, stripping incompatible flags
+    local CLEAN_CFLAGS="${RAW_CFLAGS//-static-libgcc/}"
+    CLEAN_CFLAGS="${CLEAN_CFLAGS//-static-libstdc++/}"
+    local CLEAN_CXXFLAGS="${RAW_CXXFLAGS//-static-libgcc/}"
+    CLEAN_CXXFLAGS="${CLEAN_CXXFLAGS//-static-libstdc++/}"
+    local CLEAN_LDFLAGS="${RAW_LDFLAGS//-static-libgcc/}"
+    CLEAN_LDFLAGS="${CLEAN_LDFLAGS//-static-libstdc++/}"
+
+    local CLANG_CFLAGS="$CLEAN_CFLAGS $FF_CFLAGS --target=$FFBUILD_TOOLCHAIN -Wno-unused-command-line-argument"
+    local CLANG_CXXFLAGS="$CLEAN_CXXFLAGS $FF_CXXFLAGS --target=$FFBUILD_TOOLCHAIN -Wno-unused-command-line-argument"
+    local CLANG_LDFLAGS="$CLEAN_LDFLAGS --target=$FFBUILD_TOOLCHAIN --gcc-toolchain=/opt/ct-ng -fuse-ld=lld"
+
     if [[ $TARGET == win* ]]; then
-        LTO_FLAG="-DSVT_AV1_LTO=OFF"
+        # Override sysroot for MinGW and add GCC libgcc path
+        local GCC_LIB_DIR="$(dirname $(${FFBUILD_TOOLCHAIN}-gcc -print-libgcc-file-name))"
+        echo "set(CMAKE_SYSROOT /opt/ct-ng/${FFBUILD_TOOLCHAIN}/sysroot/mingw)" >> "$PWD/clang_toolchain.cmake"
+        CLANG_CFLAGS="$CLANG_CFLAGS --sysroot=/opt/ct-ng/$FFBUILD_TOOLCHAIN/sysroot/mingw -Qunused-arguments"
+        CLANG_CXXFLAGS="$CLANG_CFLAGS"
+        CLANG_LDFLAGS="$CLANG_LDFLAGS --sysroot=/opt/ct-ng/$FFBUILD_TOOLCHAIN/sysroot/mingw -L$GCC_LIB_DIR -L/opt/ct-ng/$FFBUILD_TOOLCHAIN/sysroot/lib"
+    else
+        CLANG_CFLAGS="$CLANG_CFLAGS --gcc-toolchain=/opt/ct-ng"
+        CLANG_CXXFLAGS="$CLANG_CXXFLAGS --gcc-toolchain=/opt/ct-ng"
     fi
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=ON "$LTO_FLAG" ..
+    export CFLAGS="$CLANG_CFLAGS"
+    export CXXFLAGS="$CLANG_CXXFLAGS"
+    export LDFLAGS="$CLANG_LDFLAGS"
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$PWD/clang_toolchain.cmake" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=ON -DSVT_AV1_LTO=OFF ..
     make -j$(nproc)
     make install DESTDIR="$FFBUILD_DESTDIR"
 }
