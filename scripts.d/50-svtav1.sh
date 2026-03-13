7a6efe525471688044d25867b64d79055a86e592
DaniloJacques <danilo.jacques@gmail.com>
2026-03-13 06:38:44 -0300

    test: isolate SVT-AV1 compile to Clang Thin-LTO on zen3 toolchain

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -16,13 +16,33 @@ ffbuild_dockerdl() {
 ffbuild_dockerbuild() {
     mkdir build && cd build
 
-    local LTO_FLAG="-DSVT_AV1_LTO=ON"
+    cp "$FFBUILD_CMAKE_TOOLCHAIN" "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_C_COMPILER clang)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_CXX_COMPILER clang++)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_AR llvm-ar)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_RANLIB llvm-ranlib)' >> "$PWD/clang_toolchain.cmake"
+    echo 'set(CMAKE_NM llvm-nm)' >> "$PWD/clang_toolchain.cmake"
+
+    # Build clean CFLAGS for Clang
+    local CLANG_CFLAGS="-I/opt/ffbuild/include -O2 -pipe -fPIC -DPIC --target=$FFBUILD_TOOLCHAIN --gcc-toolchain=/opt/ct-ng -flto=thin -Wno-unused-command-line-argument"
+    local CLANG_CXXFLAGS="$CLANG_CFLAGS"
+    local CLANG_LDFLAGS="-L/opt/ffbuild/lib -O2 -pipe --target=$FFBUILD_TOOLCHAIN --gcc-toolchain=/opt/ct-ng -flto=thin -fuse-ld=lld -static-libgcc -static-libstdc++"
+
     if [[ $TARGET == win* ]]; then
-        LTO_FLAG="-DSVT_AV1_LTO=OFF"
+        # Override sysroot for MinGW and add GCC libgcc path
+        local GCC_LIB_DIR="$(dirname $(${FFBUILD_TOOLCHAIN}-gcc -print-libgcc-file-name))"
+        echo "set(CMAKE_SYSROOT /opt/ct-ng/${FFBUILD_TOOLCHAIN}/sysroot/mingw)" >> "$PWD/clang_toolchain.cmake"
+        CLANG_CFLAGS="$CLANG_CFLAGS --sysroot=/opt/ct-ng/$FFBUILD_TOOLCHAIN/sysroot/mingw"
+        CLANG_CXXFLAGS="$CLANG_CFLAGS"
+        CLANG_LDFLAGS="$CLANG_LDFLAGS --sysroot=/opt/ct-ng/$FFBUILD_TOOLCHAIN/sysroot/mingw -L$GCC_LIB_DIR -L/opt/ct-ng/$FFBUILD_TOOLCHAIN/sysroot/lib"
     fi
 
-    cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=OFF "$LTO_FLAG" ..
+    export CFLAGS="$CLANG_CFLAGS"
+    export CXXFLAGS="$CLANG_CXXFLAGS"
+    export LDFLAGS="$CLANG_LDFLAGS"
+
+    cmake -DCMAKE_TOOLCHAIN_FILE="$PWD/clang_toolchain.cmake" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=OFF -DSVT_AV1_LTO=OFF ..
     make -j$(nproc)
     make install DESTDIR="$FFBUILD_DESTDIR"
 }
