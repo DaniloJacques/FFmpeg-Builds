60b1a5bb4af4dcc8faf52d26bdf40edfe5f295d9
DaniloJacques <danilo.jacques@gmail.com>
2026-03-15 18:36:42 -0300

    revert(x265): fallback to native GCC due to severe clang obj performance drop without LTO

--- scripts.d/50-x265.sh
+++ scripts.d/50-x265.sh
@@ -23,22 +23,9 @@ ffbuild_dockerbuild() {
         -DENABLE_ALPHA=ON
     )
 
-    # Injetar temporariamente binários do LLVM-21 (Targeted Clang Phase 1)
-    export PATH="/opt/llvm-21/bin:$PATH"
-
-    if [[ $TARGET == win64 ]]; then
-        export CC="clang --target=${FFBUILD_TOOLCHAIN} --sysroot=/opt/ct-ng/${FFBUILD_TOOLCHAIN}/sysroot/mingw"
-        export CXX="clang++ --target=${FFBUILD_TOOLCHAIN} --sysroot=/opt/ct-ng/${FFBUILD_TOOLCHAIN}/sysroot/mingw"
-        export LD="clang --target=${FFBUILD_TOOLCHAIN} --sysroot=/opt/ct-ng/${FFBUILD_TOOLCHAIN}/sysroot/mingw -fuse-ld=lld"
-    else
-        export CC="clang --target=${FFBUILD_TOOLCHAIN} --gcc-toolchain=/opt/ct-ng"
-        export CXX="clang++ --target=${FFBUILD_TOOLCHAIN} --gcc-toolchain=/opt/ct-ng"
-        export LD="clang --target=${FFBUILD_TOOLCHAIN} --gcc-toolchain=/opt/ct-ng -fuse-ld=lld"
-    fi
-
-    export CFLAGS="$RAW_CFLAGS -fno-lto"
-    export CXXFLAGS="$RAW_CXXFLAGS -fno-lto"
-    export LDFLAGS="$RAW_LDFLAGS -fno-lto"
+    export CFLAGS="$RAW_CFLAGS -flto"
+    export CXXFLAGS="$RAW_CXXFLAGS -flto"
+    export LDFLAGS="$RAW_LDFLAGS -flto"
 
     sed -i '1i#include <cstdint>' source/dynamicHDR10/json11/json11.cpp
 
