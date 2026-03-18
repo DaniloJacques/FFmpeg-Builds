a25fc6524f0ce46cbda393ab7db32e06a83a7318
DaniloJacques <danilo.jacques@gmail.com>
2026-03-18 09:43:13 -0300

    feat(x265): upstream x265 to latest master commit 8be7dbf

--- scripts.d/50-x265.sh
+++ scripts.d/50-x265.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://bitbucket.org/multicoreware/x265_git.git"
-SCRIPT_COMMIT="50087d0b879961afff20f76e2a841eaca301b33e"
+SCRIPT_COMMIT="8be7dbf8159ddfceea4115675a6d48e1611b8baa"
 
 ffbuild_enabled() {
     [[ $VARIANT == lgpl* ]] && return -1
